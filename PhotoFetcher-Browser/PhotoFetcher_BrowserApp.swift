d97b1bbaa6ff20909c4c84a9f009edd782f40bf4
lukaszbielawski <lukasz.bi@onet.pl>
2023-08-16 10:17:58 +0200

    Initial Commit

---
+++ PhotoFetcher-Browser/PhotoFetcher_BrowserApp.swift
@@ -0,0 +1,17 @@
+//
+//  PhotoFetcher_BrowserApp.swift
+//  PhotoFetcher-Browser
+//
+//  Created by Łukasz Bielawski on 16/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct PhotoFetcher_BrowserApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
